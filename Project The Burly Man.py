887b9a010358542f4dd5dcd3dfb79da17c4e0a82
SeanGay4 <seangay30@gmail.com>
2023-10-17 08:31:00 -0400

    Add description and welcome print lines

---
+++ Project The Burly Man.py
@@ -0,0 +1,8 @@
+"""
+ Welcome Screen will start our program letting
+drivers know that the InfoTech Center 2023 is loading
+"""
+
+print("\nWelcome - Infotech Center 2023")
+
+print("\nInfoTech Center 2023 is loading...")
