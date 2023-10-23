370ccfffa188a467565abc382b8123bf49d0fa3b
SeanGay4 <seangay30@gmail.com>
2023-10-23 08:29:49 -0400

    Adding the timetosleep variable and making it stable

--- Project The Burly Man.py
+++ Project The Burly Man.py
@@ -3,6 +3,11 @@
 drivers know that the InfoTech Center 2023 is loading
 """
 
-print("\nWelcome - Infotech Center 2023")
+# Import Libraries Here
+import time
 
+timetosleep = 2
+
+print("\nWelcome - Infotech Center 2023")
+time.sleep(timetosleep)
 print("\nInfoTech Center 2023 is loading...")
